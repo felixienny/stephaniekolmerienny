7c21f91d64e54466df58932e5c5d781f9a5e2bda
Felix IENNY <felix.ienny5@gmail.com>
2025-08-17 11:18:25 +0200

    Update asset manifest and index.html for new JavaScript bundle; simplify routing in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+const queryParams = new URLSearchParams(window.location.search);
+const rawJob = queryParams.get("job");
+
+const job = (rawJob === "droit" || rawJob === "mediation") ? rawJob : "droit";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/droit" element={<App job="droit" />} />
-        <Route path="/mediation" element={<App job="mediation" />} />
-        <Route path="*" element={<Navigate replace to={'/droit'} />} />
-      </Routes>
-    </HashRouter>
+    <App job={job} />
   </React.StrictMode>
 );
