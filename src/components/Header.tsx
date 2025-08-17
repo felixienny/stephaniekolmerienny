7da035989464ad11efcceb9916b9d2304837a8a2
Felix IENNY <felix.ienny5@gmail.com>
2025-08-17 11:29:31 +0200

    update href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
 
-function Header(props: { job: "droit" | "mediation" }) {
+function Header(props: { job: 'droit' | 'mediation' }) {
   return (
     <div className="fixed-top header">
       <ul className="nav pt-2 justify-content-center">
         <li className="nav-item">
-          <NavLink to="/droit" className={`nav-link button-avocat`}>
+          <a href="?job=droit" className="nav-link button-avocat">
             Avocat
-          </NavLink>
+          </a>
         </li>
         <li className="nav-item">
-          <NavLink to="/mediation" className={`nav-link button-mediateur`}>
+          <a href="?job=mediation" className="nav-link button-mediateur">
             Médiatrice
-          </NavLink>
+          </a>
         </li>
       </ul>
 
@@ -21,7 +20,7 @@ function Header(props: { job: "droit" | "mediation" }) {
         height="750"
         className="navbar-logo mx-auto d-block"
         src={
-          props.job === "droit" ? "/melkor_droit.png" : "/melkor_meditation.png"
+          props.job === 'droit' ? '/melkor_droit.png' : '/melkor_meditation.png'
         }
         alt=""
       />
