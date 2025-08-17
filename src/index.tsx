f7b64514a5c59fd858860969aef2213bd689cd36
Felix IENNY <felix.ienny5@gmail.com>
2025-08-17 10:59:41 +0200

    Update asset manifest and switch to HashRouter; remove old CNAME and license files

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/droit" element={<App job="droit" />} />
         <Route path="/mediation" element={<App job="mediation" />} />
-        <Route path="*" element={<Navigate replace to={"/droit"} />} />
+        <Route path="*" element={<Navigate replace to={'/droit'} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
