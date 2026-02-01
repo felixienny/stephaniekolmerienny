6ec7268071e66f9a67fa5ebf44198300ddb87e80
Felix IENNY <fienny@hexanet.fr>
2026-02-01 17:43:31 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 
 function About() {
-  return (
-    <div className="About container text-justify p-4">
-      <div className="grid row">
-        <div className="col-lg-4">
-          <img
-            src="/stephanie_kolmer_ienny.jpg"
-            alt="Stephanie Kolmer Ienny"
-            className="img-fluid rounded mx-auto d-block"
-          />
+    return (
+        <div className="About container text-justify p-4">
+            <div className="grid row">
+                <div className="col-lg-4">
+                    <img
+                        src="/stephanie_kolmer_ienny.jpg"
+                        alt="Stephanie Kolmer Ienny"
+                        className="img-fluid rounded mx-auto d-block"
+                    />
+                </div>
+                <div className="col-lg-8 pt-2">
+                    <h2>Stéphanie KOLMER-IENNY</h2>
+                    <p>Maître Stéphanie KOLMER-IENNY est avocat inscrit au Barreau de
+                        Reims. Ses domaines de compétences principaux sont en Droit du
+                        travail et social et Droit de la famille, des personnes et de leur
+                        patrimoine.</p>
+                    <p>Elle intervient à la fois comme conseil
+                        en amont des conflits, et comme avocat chargé d'assurer la défense
+                        de vos intérêts devant les tribunaux, que ce soit en défense, ou
+                        pour engager une procédure contre l'adversaire.</p>
+                </div>
+            </div>
         </div>
-        <div className="col-lg-8 pt-2">
-          <h2>Stéphanie KOLMER-IENNY</h2>
-          <p>
-            Maître Stéphanie KOLMER-IENNY est avocat inscrit au Barreau de
-            Reims. Ses domaines de compétences principaux sont en Droit du
-            travail et social et Droit de la famille, des personnes et de leur
-            patrimoine. Maître KOLMER-IENNY intervient à la fois comme conseil
-            en amont des conflits, et comme avocat chargé d'assurer la défense
-            de vos intérêts devant les tribunaux, que ce soit en défense, ou
-            pour engager une procédure contre l'adversaire.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default About;
