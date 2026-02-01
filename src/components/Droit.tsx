9113c3e489634a2b00263afd72ddb5e6f4faba3a
Felix IENNY <fienny@hexanet.fr>
2026-02-01 16:32:52 +0100

    update Droit component content to reflect legal expertise and services

--- src/components/Droit.tsx
+++ src/components/Droit.tsx
@@ -1,37 +1,21 @@
 import React from "react";
 
 function Droit() {
-  return (
-    <div className="Overview">
-      <h1>Avocat</h1>
+    return (
+        <div className="Overview">
+            <h1>Avocat</h1>
 
-      <div>
-        <p>
-          Stéphanie KOLMER-IENNY est également avocate spécialisée en droit de
-          la famille et du travail. Dans ce rôle, elle aide les clients à
-          résoudre les problèmes juridiques liés à la famille et au travail,
-          tels que les divorce, la garde d'enfants, les litiges relatifs à la
-          pension alimentaire, les questions de harcèlement au travail, etc.
-          Stéphanie est reconnue pour son approche personnalisée et attentive à
-          chaque client, ainsi que pour sa compréhension approfondie des lois et
-          des politiques en matière de droit de la famille et du travail. Elle
-          utilise ses connaissances et son expérience pour aider les clients à
-          trouver des solutions qui répondent à leurs besoins uniques et qui
-          sont conformes à la loi. En tant qu'avocate, Stéphanie se tient au
-          courant des dernières développements dans son domaine et s'engage à
-          fournir des conseils juridiques clairs et précis à ses clients. Elle
-          s'efforce de les aider à naviguer dans le système judiciaire et à
-          défendre leurs droits avec succès. En résumé, le métier d'avocate en
-          droit de la famille et du travail requiert une connaissance
-          approfondie des lois, une capacité à communiquer clairement, une
-          grande capacité d'analyse et une capacité à trouver des solutions
-          créatives aux problèmes juridiques complexes. Stéphanie KOLMER-IENNY
-          possède toutes ces qualités et plus encore, ce qui en fait une avocate
-          compétente et dévouée pour ses clients.
-        </p>
-      </div>
-    </div>
-  );
+            <div>
+                <p>Maître Stéphanie KOLMER-IENNY est avocat depuis 1992.</p>
+                <p>Dans le cadre d’une activité généraliste, elle développe notamment ses compétences en droit de la famille, des personnes et de leur patrimoine (divorce, séparation, garde d’enfants séparations, fixation de pension alimentaire, successions), ainsi que dans le contentieux civil et commercial des contrats et de la responsabilité.</p>
+                <p>A ce titre, elle intervient devant de nombreuses juridictions : Juge aux affaires familiales, Juge de l’exécution, Juge des Contentieux de la Protection, Tribunal de Commerce, Conseil de Prud’hommes.</p>
+                <p>Maître KOLMER-IENNY garantit la défense des intérêts des particuliers et des entreprises.</p>
+                <p>Elle les représente et les assiste devant les Tribunaux mais elle les conseille également en amont des contentieux afin d’anticiper ou d’éviter les procédures judiciaires.</p>
+                <p>Elle est attentive aux besoins et attente de chaque client et privilégie une approche personnalisée de ses dossiers.</p>
+                <p>Elle déploie sa compétence, son humanité et son efficacité au service de ses clients.</p>
+            </div>
+        </div>
+    );
 }
 
 export default Droit;
